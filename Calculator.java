b9c4a08647d6f5a419ea40b79a15be5a2c72c2f3
lily1474 <57844461+lily1474@users.noreply.github.com>
2020-03-09 11:40:41 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args)
+    { Scanner Keyboard = new Scanner(System.in);
+        System.out.println("Calculator Menu");
+        System.out.println("---------------");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("\n");
+        System.out.println("Which operation do you want to perform?");
+
+    }
+}
+
